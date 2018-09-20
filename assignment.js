f688ac06376765cf9522b2b176419165b5e14ea6
SimonSun <yans3@illinois.edu>
2018-09-19 20:04:58 -0500

    finished

---
+++ assignment.js
@@ -0,0 +1,12 @@
+let array = [1,2,3,4,5]
+console.log(array)
+let Assignment1 = array.map(elem => elem * 3)
+console.log(Assignment1)
+
+let array2 = ["hi ","my ", "name ","is ","Simon"]
+let a = ""
+for(var i = 0; i<array2.length; i++) {
+  a = a + array2[i]
+}
+console.log(array2);
+console.log(a);
